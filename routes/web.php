634527bd1e9ba8be7e2d9f17eaf8ccf31a3b0188
jazielg <jazielgj1995@gmail.com>
2019-01-29 15:55:04 -0200

    adiciona, mvc e eloquent

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
+
+Route::get('/projects', 'ProjectsController@index');
