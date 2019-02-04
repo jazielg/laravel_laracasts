0ce5e1eb6e09a14542fb3dd2d09ceea514742fa3
jazielg <jazielgj1995@gmail.com>
2019-02-04 18:15:14 -0200

    adiciona, front-end

--- resources/views/projectslayout.blade.php
+++ resources/views/projectslayout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Projects</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <style>
         .is-completed {
             text-decoration: line-through;
@@ -17,6 +17,10 @@
     <div class="container">
         @yield('content')
     </div>
+
+<script src="{{ mix('js/app.js') }}"></script>
+<script src="{{ mix('js/vendor.js') }}"></script>
+<script src="{{ mix('js/manifest.js') }}"></script>
     
 </body>
 </html>
